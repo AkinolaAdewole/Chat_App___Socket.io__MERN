fed550ac8fe93d56625de555e6fff2c9c2e073ee
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-13 00:02:48 +0100

    client

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -4,16 +4,22 @@ import FollowersModal from "../FollowersModal/FollowersModal";
 import { getAllUser } from "../../api/UserRequests";
 import User from "../User/User";
 import { useSelector } from "react-redux";
+
 const FollowersCard = ({ location }) => {
+  // Define state variables to manage the visibility of the followers modal and the list of persons to display
   const [modalOpened, setModalOpened] = useState(false);
   const [persons, setPersons] = useState([]);
+  
+  // Get the user data from the Redux store using the useSelector hook
   const { user } = useSelector((state) => state.authReducer.authData);
 
   useEffect(() => {
+    // Fetch a list of persons (users) using an API request
     const fetchPersons = async () => {
       const { data } = await getAllUser();
       setPersons(data);
     };
+    // Call the fetchPersons function when the component mounts
     fetchPersons();
   }, []);
 
@@ -22,14 +28,17 @@ const FollowersCard = ({ location }) => {
       <h3>People you may know</h3>
 
       {persons.map((person, id) => {
+        // Render a User component for each person (user) in the list, excluding the current user
         if (person._id !== user._id) return <User person={person} key={id} />;
       })}
+      {/* Render a "Show more" link if the location prop is not provided */}
       {!location ? (
         <span onClick={() => setModalOpened(true)}>Show more</span>
       ) : (
         ""
       )}
 
+      {/* FollowersModal component for displaying more followers */}
       <FollowersModal
         modalOpened={modalOpened}
         setModalOpened={setModalOpened}
--- client/src/components/FollowersModal/FollowersModal.jsx
+++ client/src/components/FollowersModal/FollowersModal.jsx
@@ -3,9 +3,12 @@ import { Modal, useMantineTheme } from "@mantine/core";
 import FollowersCard from "../FollowersCard/FollowersCard";
 
 const FollowersModal = ({ modalOpened, setModalOpened }) => {
+  // Access the Mantine theme
   const theme = useMantineTheme();
+
   return (
     <Modal
+      // Configure modal appearance based on the theme and other properties
       overlayColor={
         theme.colorScheme === "dark"
           ? theme.colors.dark[9]
@@ -14,11 +17,13 @@ const FollowersModal = ({ modalOpened, setModalOpened }) => {
       overlayOpacity={0.55}
       overlayBlur={3}
       size="55%"
+      // Control modal visibility using the `modalOpened` prop
       opened={modalOpened}
+      // Define an event handler to close the modal when clicked
       onClose={() => setModalOpened(false)}
     >
-
-    <FollowersCard location='modal'/>
+      {/* Render the FollowersCard component within the modal */}
+      <FollowersCard location="modal" />
     </Modal>
   );
 };
