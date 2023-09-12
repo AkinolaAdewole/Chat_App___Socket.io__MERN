cba50ff21e960e17384085e2943e430eede354e7
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 21:25:49 +0100

    client

--- client/src/components/Coverstion/Converstion.jsx
+++ client/src/components/Coverstion/Converstion.jsx
@@ -1,45 +1,53 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getUser } from "../../api/UserRequests";
+import { getUser } from "../../api/UserRequests"; // Importing a function for getting user data
 
 const Conversation = ({ data, currentUser, online }) => {
+  // Define local state to store user data
+  const [userData, setUserData] = useState(null);
 
-  const [userData, setUserData] = useState(null)
-  const dispatch = useDispatch()
+  // Redux dispatch hook for dispatching actions
+  const dispatch = useDispatch();
 
-  useEffect(()=> {
+  useEffect(() => {
+    // Extracting the user ID from the conversation data
+    const userId = data.members.find((id) => id !== currentUser);
 
-    const userId = data.members.find((id)=>id!==currentUser)
-    const getUserData = async ()=> {
-      try
-      {
-          const {data} =await getUser(userId)
-         setUserData(data)
-         dispatch({type:"SAVE_USER", data:data})
+    // Function to fetch user data
+    const getUserData = async () => {
+      try {
+        const { data } = await getUser(userId); // Making an API request to get user data
+        setUserData(data); // Updating local state with received user data
+        dispatch({ type: "SAVE_USER", data: data }); // Dispatching a Redux action to save user data
+      } catch (error) {
+        console.log(error); // Handle errors, e.g., log them or show a notification
       }
-      catch(error)
-      {
-        console.log(error)
-      }
-    }
+    };
 
-    getUserData();
-  }, [])
+    getUserData(); // Call the function to fetch user data
+  }, []); // Empty dependency array indicates that this effect runs once on component mount
+
   return (
     <>
       <div className="follower conversation">
         <div>
-          {online && <div className="online-dot"></div>}
+          {online && <div className="online-dot"></div>} {/* Render an online dot if the user is online */}
           <img
-            src={userData?.profilePicture? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"}
+            src={
+              userData?.profilePicture
+                ? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture
+                : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"
+            }
             alt="Profile"
             className="followerImage"
             style={{ width: "50px", height: "50px" }}
           />
-          <div className="name" style={{fontSize: '0.8rem'}}>
-            <span>{userData?.firstname} {userData?.lastname}</span>
-            <span style={{color: online?"#51e200":""}}>{online? "Online" : "Offline"}</span>
+          <div className="name" style={{ fontSize: "0.8rem" }}>
+            <span>{userData?.firstname} {userData?.lastname}</span> {/* Render user's first and last name */}
+            <span style={{ color: online ? "#51e200" : "" }}>
+              {online ? "Online" : "Offline"} {/* Display user's online status */}
+            </span>
           </div>
         </div>
       </div>
---
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import "./InfoCard.css";
+import { UilPen } from "@iconscout/react-unicons"; // Icon component for editing
+import ProfileModal from "../ProfileModal/ProfileModal"; // Modal for editing user profile
+import { useDispatch, useSelector } from "react-redux"; // Redux hooks for state management
+import { useParams } from "react-router-dom"; // React Router hook for accessing URL parameters
+import * as UserApi from "../../api/UserRequests.js"; // API functions for user-related requests
+import { logout } from "../../actions/AuthActions"; // Redux action for user logout
+
+const InfoCard = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [modalOpened, setModalOpened] = useState(false); // State for controlling the profile edit modal
+  const profileUserId = params.id; // Extract the user ID from the URL parameter
+  const [profileUser, setProfileUser] = useState({}); // State to store the profile user's data
+  const { user } = useSelector((state) => state.authReducer.authData); // Get the authenticated user's data from Redux
+
+  // Function to handle user logout
+  const handleLogOut = () => {
+    dispatch(logout());
+  };
+
+  // useEffect to fetch the profile user's data when the component mounts
+  useEffect(() => {
+    const fetchProfileUser = async () => {
+      if (profileUserId === user._id) {
+        // If the profile being viewed is the authenticated user's own profile
+        setProfileUser(user);
+      } else {
+        // If viewing another user's profile, fetch their data from the API
+        console.log("fetching");
+        const profileUser = await UserApi.getUser(profileUserId);
+        setProfileUser(profileUser);
+        console.log(profileUser);
+      }
+    };
+    fetchProfileUser();
+  }, [user, profileUserId]); // useEffect depends on 'user' and 'profileUserId'
+
+  return (
+    <div className="InfoCard">
+      <div className="infoHead">
+        <h4>Profile Info</h4>
+        {user._id === profileUserId ? (
+          // Display an edit icon and a profile edit modal for the authenticated user's own profile
+          <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+              data={user} // Pass user data to the profile edit modal
+            />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Status </b>
+        </span>
+        <span>{profileUser.relationship}</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Lives in </b>
+        </span>
+        <span>{profileUser.livesIn}</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>{profileUser.worksAt}</span>
+      </div>
+
+      <button className="button logout-button" onClick={handleLogOut}>
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default InfoCard;
