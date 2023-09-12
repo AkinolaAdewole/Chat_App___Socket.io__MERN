647d6398e49627a1b7c53f9bdb54033161fe205e
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 23:54:41 +0100

    client

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,3 +1,4 @@
+// Import necessary dependencies and components
 import React, { useEffect } from "react";
 import { getTimelinePosts } from "../../actions/PostsAction";
 import Post from "../Post/Post";
@@ -5,16 +6,26 @@ import { useSelector, useDispatch } from "react-redux";
 import "./Posts.css";
 import { useParams } from "react-router-dom";
 
+// Define the Posts component
 const Posts = () => {
+  // Get route parameters and initialize dispatch
   const params = useParams()
   const dispatch = useDispatch();
+  
+  // Select user data and posts from Redux store
   const { user } = useSelector((state) => state.authReducer.authData);
   let { posts, loading } = useSelector((state) => state.postReducer);
+  
+  // Fetch timeline posts on component mount
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
+  
+  // Filter posts based on route parameters if id is provided
   if(!posts) return 'No Posts';
   if(params.id) posts = posts.filter((post)=> post.userId===params.id)
+  
+  // Render the Posts component
   return (
     <div className="Posts">
       {loading
@@ -26,4 +37,5 @@ const Posts = () => {
   );
 };
 
+// Export the Posts component
 export default Posts;
--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -6,20 +6,23 @@ import ShareModal from "../ShareModal/ShareModal";
 import NavIcons from "../NavIcons/NavIcons";
  
 const RightSide = () => {
+  // Define a state variable to manage the visibility of the share modal
   const [modalOpened, setModalOpened] = useState(false);
 
   return (
     <div className="RightSide">
       {/* Side Navbar */}
-
       <NavIcons />
+      
       {/* TrendCard */}
       <TrendCard />
 
-      {/* Share buttong */}
+      {/* Share button */}
       <button className="button r-button" onClick={() => setModalOpened(true)}>
         Share
       </button>
+      
+      {/* ShareModal component */}
       <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   );
