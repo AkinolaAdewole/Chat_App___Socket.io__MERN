0ac51d62d704b8274e18f56c6a75a371b7c30ceb
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 22:57:20 +0100

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth/Auth";
+import Profile from "./pages/Profile/Profile";
+import { useSelector } from "react-redux";
+import Chat from "./pages/Chat/Chat";
 
 function App() {
+  // Get user authentication data from Redux store
+  const user = useSelector((state) => state.authReducer.authData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        height:
+          window.location.href === "http://localhost:3000/chat"
+            ? "calc(100vh - 2rem)"
+            : "auto",
+      }}
+    >
+      {/* Add background blur effects */}
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+
+      {/* Define routes using React Router */}
+      <Routes>
+        {/* Default route based on user authentication status */}
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+
+        {/* Home page */}
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+
+        {/* Authentication page */}
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+
+        {/* User profile page */}
+        <Route
+          path="/profile/:id"
+          element={user ? <Profile /> : <Navigate to="../auth" />}
+        />
+
+        {/* Catch-all route for undefined routes */}
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+
+        {/* Chat page */}
+        <Route
+          path="/chat"
+          element={user ? <Chat /> : <Navigate to="../auth" />}
+        />
+      </Routes>
     </div>
   );
 }
