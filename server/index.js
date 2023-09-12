a862a21b53ebe422d7d8c65c9fae7f56f63cee8d
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 16:03:43 +0100

    Controllers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
+
+//! MiddleWare
+app.use(bodyParser.json({ limit : "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 
 // routes
