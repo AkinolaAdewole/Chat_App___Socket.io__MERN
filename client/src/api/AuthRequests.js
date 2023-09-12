65aa63d3338846a1f6afefdde84b55a30e7c2d7a
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 21:05:13 +0100

    client

--- client/src/api/AuthRequests.js
+++ client/src/api/AuthRequests.js
@@ -1,8 +1,11 @@
-import axios from 'axios'
+// Import the Axios library for making HTTP requests
+import axios from 'axios';
 
-
+// Create an Axios instance with a base URL for the server
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-export const logIn= (formData)=> API.post('/auth/login',formData);
+// Function to log in a user
+export const logIn = (formData) => API.post('/auth/login', formData);
 
+// Function to sign up a new user
 export const signUp = (formData) => API.post('/auth/register', formData);
